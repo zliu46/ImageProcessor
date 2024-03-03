bfd9c09e7bc3e2d9e45cc24b9d376a0fcde2a083
Zhou Liu <111816533+zliu46@users.noreply.github.com>
2024-03-02 20:40:32 -0800

    Add files via upload

---
+++ PikasoServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using PikasoServer.Services;
+using static System.Net.WebRequestMethods;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup a HTTP/2 endpoint without TLS.
+    options.ListenLocalhost(5161, o => o.Protocols =
+        HttpProtocols.Http2);
+});
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Make sure to register the ImageProcessorService
+app.MapGrpcService<ImageProcessorService>();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
+
---
+++ PikasoServer/Services/ImageProcessorService.cs
@@ -0,0 +1,69 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using PikasoServer; // Adjust this using the correct namespace for your generated proto code
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PikasoServer.Services
+{
+    public class ImageProcessorService : ImageProcessor.ImageProcessorBase
+    {
+        public override async Task<ImageProcessResponse> ProcessImage(ImageProcessRequest request, ServerCallContext context)
+        {
+            using var image = Image.Load(request.Image.ToByteArray());
+            if (request.ConvertToGrayscale)
+            {
+                image.Mutate(x => x.Grayscale());
+            }
+
+            // Flip horizontally if requested
+            if (request.FlipHorizontal)
+            {
+                image.Mutate(x => x.Flip(FlipMode.Horizontal));
+            }
+
+            // Flip vertically if requested
+            if (request.FlipVertical)
+            {
+                image.Mutate(x => x.Flip(FlipMode.Vertical));
+            }
+
+            // Rotate the image by the specified degrees
+            if (request.RotateDegrees != 0)
+            {
+                image.Mutate(x => x.Rotate(request.RotateDegrees));
+            }
+
+            // Resize the image if requested
+            if (request.ResizeWidth > 0 && request.ResizeHeight > 0)
+            {
+                image.Mutate(x => x.Resize(request.ResizeWidth, request.ResizeHeight));
+            }
+
+            // Generate a thumbnail if requested
+            if (request.GenerateThumbnail)
+            {
+                // Define thumbnail size, you can adjust these values
+                const int thumbnailWidth = 200;
+                const int thumbnailHeight = 200;
+                image.Mutate(x => x.Resize(thumbnailWidth, thumbnailHeight));
+            }
+
+            // Save the processed image to a memory stream
+            using (var ms = new MemoryStream())
+            {
+                image.SaveAsJpeg(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                // Return the processed image
+                return new ImageProcessResponse
+                {
+                    Image = ByteString.CopyFrom(ms.ToArray())
+                };
+            }
+        }
+    }
+}
